8bbdfb2a31bf8f70c3659fe4bc34311e287d16ac
Yusufalade2 <yusufalade1234@gmail.com>
2023-02-26 06:26:23 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints character of i
+* 
+* @i: - uses _putchar function to print i
+*
+* Return: returns characrer on success
+*/
+
+int _putchar(char i)
+{
+	return (write(1, &i, 1 ));
+}

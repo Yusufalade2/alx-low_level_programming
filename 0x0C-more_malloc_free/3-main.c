4e9380b431f190016bd596c8e93f21ff5410c3ee
Yusufalade2 <yusufalade1234@gmail.com>
2023-03-16 17:35:35 -0700

    3-array_range

--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -38,3 +38,10 @@ void simple_print_buffer(int *buffer, unsigned int size)
  */
 int main(void)
 {
+    int *a;
+
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
+}
